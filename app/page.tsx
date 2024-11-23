32fab9e9676885842326116da9972971e18f3e39
ariadiprana <ari.adiprana@gmail.com>
2024-11-23 18:57:07 +0700

    ESLint & Prettier Fixing

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
-import Image from "next/image";
-
 const Home = () => (
-  <h1 className="text-3xl text-white font-black">
+  <h1 className="text-3xl font-black text-white">
     Welcome to the world of next.js
   </h1>
 );
