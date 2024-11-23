bf9dd584c36633a4bc336754f68e5ad5bb80d734
ariadiprana <ari.adiprana@gmail.com>
2024-11-23 22:26:28 +0700

    Assets & Metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,28 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
-import "./globals.css";
 import React from "react";
+
+import "./globals.css";
 
 const inter = localFont({
   src: "./fonts/InterVF.ttf",
   variable: "--font-inter",
-  weight: "100 200 300 400 500 600 700 800 900",
+  weight: "100 200 300 400 500 700 800 900",
 });
 
 const spaceGrotesk = localFont({
   src: "./fonts/SpaceGroteskVF.ttf",
   variable: "--font-space-grotesk",
-  weight: "300 400 500 600 700",
+  weight: "300 400 500 700",
 });
 
 export const metadata: Metadata = {
   title: "DevFlow",
-  description: "A better version of Stack Overflaw",
+  description:
+    "A community-driven platform for asking and answering programming questions. Get help, share knowledge, and collaborate with developers from around the world. Explore topics in web development, mobile app development, algorithms, data structures, and more.",
+  icons: {
+    icon: "/images/site-logo.svg",
+  },
 };
 
 export default function RootLayout({
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,7 @@
-const Home = () => {
-  return (
-    <>
-      <h1 className="h1-bold">Welcome to the world of next.js</h1>
-      <h1 className="h1-bold font-space-grotesk">
-        Welcome to the world of next.js (INTER)
-      </h1>
-    </>
-  );
-};
+const Home = () => (
+  <>
+    <h1 className="h1-bold">Welcome to the world of Next.js</h1>
+  </>
+);
+
 export default Home;
