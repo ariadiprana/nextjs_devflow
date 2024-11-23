4bffabe54a5dee307dbd47c6a22f2bde6b808950
ariadiprana <ari.adiprana@gmail.com>
2024-11-23 21:57:41 +0700

    Font Setup

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,9 @@ const Home = () => {
   return (
     <>
       <h1 className="h1-bold">Welcome to the world of next.js</h1>
-      <h2 className="h2-bold">Hahaha</h2>
+      <h1 className="h1-bold font-space-grotesk">
+        Welcome to the world of next.js (INTER)
+      </h1>
     </>
   );
 };
