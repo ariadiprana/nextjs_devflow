1c1e468f21f4c231db768fa8b6dd11a6b05e6ee4
ariadiprana <ari.adiprana@gmail.com>
2024-11-24 20:02:27 +0700

    Routing Group

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SignIn = () => {
+  return <h1 className="text-3xl">Sign In</h1>;
+};
+
+export default SignIn;
---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SignUp = () => {
+  return <h1 className="text-3xl">Sign Up</h1>;
+};
+
+export default SignUp;
