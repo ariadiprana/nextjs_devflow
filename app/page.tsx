c69ee69100a27204e0716f332e683350e9f5b144
ariadiprana <ari.adiprana@gmail.com>
2024-11-25 21:35:02 +0700

    shadcn UI Implementation

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,29 @@
 import Link from "next/link";
 
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
 const Home = () => (
   <main>
     <Link href="/projects/list">See Projects</Link>
     <h1 className="h1-bold">Home</h1>
+    <DropdownMenu>
+      <DropdownMenuTrigger>Open</DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>Profile</DropdownMenuItem>
+        <DropdownMenuItem>Billing</DropdownMenuItem>
+        <DropdownMenuItem>Team</DropdownMenuItem>
+        <DropdownMenuItem>Subscription</DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
   </main>
 );
 
