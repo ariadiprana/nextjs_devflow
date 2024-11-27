03755a5f5a9e658680939e0101cdd868791d082e
ariadiprana <ari.adiprana@gmail.com>
2024-11-27 22:59:59 +0700

    Github Auth Implementation

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,25 @@
-const Home = () => (
-  <main>
-    <h1 className="h1-bold">Home</h1>
-  </main>
-);
+import { auth, signOut } from "@/auth";
+import { Button } from "@/components/ui/button";
+import ROUTES from "@/contants/routes";
+
+const Home = async () => {
+  const session = await auth();
+  console.log(session);
+
+  return (
+    <>
+      <h1 className="h1-bold">Home</h1>
+      <form
+        className="px-10 pt-[100px]"
+        action={async () => {
+          "use server";
+          await signOut({ redirectTo: ROUTES.SIGN_IN });
+        }}
+      >
+        <Button type="submit">Log out</Button>
+      </form>
+    </>
+  );
+};
 
 export default Home;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,14 @@
+/* eslint-disable import/order */
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import { ThemeProvider } from "next-themes";
 import React from "react";
 
 import "./globals.css";
+import { auth } from "@/auth";
+import { Toaster } from "@/components/ui/toaster";
+
+import { SessionProvider } from "next-auth/react";
 
 const inter = localFont({
   src: "./fonts/InterVF.ttf",
@@ -26,25 +31,28 @@ export const metadata: Metadata = {
   },
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+const RootLayout = async ({ children }: { children: React.ReactNode }) => {
+  const session = await auth();
+
   return (
     <html suppressHydrationWarning lang="en">
-      <body
-        className={`${inter.className} ${spaceGrotesk.variable} antialiased`}
-      >
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
+      <SessionProvider session={session}>
+        <body
+          className={`${inter.className} ${spaceGrotesk.variable} antialiased`}
         >
-          {children}
-        </ThemeProvider>
-      </body>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+          <Toaster />
+        </body>
+      </SessionProvider>
     </html>
   );
-}
+};
+
+export default RootLayout;
