4e3269a513d7596bbf5c5544650a3d7649305a81
ariadiprana <ari.adiprana@gmail.com>
2024-12-04 21:32:12 +0700

    Navigation Mobile

--- components/navigation/navbar/index.tsx
+++ components/navigation/navbar/index.tsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 import React from "react";
 
 import Theme from "./Theme";
+import MobileNavigation from "./MobileNavigation";
 
 const Navbar = () => {
   return (
@@ -28,6 +29,7 @@ const Navbar = () => {
       <p>Global Saerch</p>
       <div className="flex-between gap-5">
         <Theme />
+        <MobileNavigation />
       </div>
     </nav>
   );
