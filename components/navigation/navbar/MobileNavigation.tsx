bbd91dc4ce9a6f7f0553a864edc0e114453d14cb
ariadiprana <ari.adiprana@gmail.com>
2024-12-07 23:52:54 +0700

    Login Sign Up - Mobile Navigation

--- components/navigation/navbar/MobileNavigation.tsx
+++ components/navigation/navbar/MobileNavigation.tsx
@@ -60,6 +60,13 @@ const MobileNavigation = () => {
                 </Button>
               </Link>
             </SheetClose>
+            <SheetClose asChild>
+              <Link href={ROUTES.SIGN_UP}>
+                <Button className="small-medium light-border-2 btn-tertiary text-dark400_light900 min-h-[41px] w-full  rounded-lg border px-4 py-3 shadow-none">
+                  Sign Up
+                </Button>
+              </Link>
+            </SheetClose>
           </div>
         </div>
       </SheetContent>
