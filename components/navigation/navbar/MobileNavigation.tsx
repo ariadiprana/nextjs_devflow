f7ff01986697c94226802da6ed828ba759c1bb42
ariadiprana <ari.adiprana@gmail.com>
2024-12-07 07:28:44 +0700

    MobileNavigation

--- components/navigation/navbar/MobileNavigation.tsx
+++ components/navigation/navbar/MobileNavigation.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 import {
@@ -12,15 +14,35 @@ import {
 const MobileNavigation = () => {
   return (
     <Sheet>
-      <SheetTrigger>Open</SheetTrigger>
-      <SheetContent>
-        <SheetHeader>
-          <SheetTitle>Are you absolutely sure?</SheetTitle>
-          <SheetDescription>
-            This action cannot be undone. This will permanently delete your
-            account and remove your data from our servers.
-          </SheetDescription>
-        </SheetHeader>
+      <SheetTrigger asChild>
+        <Image
+          src="/icons/hamburger.svg"
+          width={36}
+          height={36}
+          alt="Menu"
+          className="invert-colors sm:hidden"
+        />
+      </SheetTrigger>
+      <SheetContent
+        side="left"
+        className="background-light900_dark00 border-none"
+      >
+        <SheetTitle className="hidden">Navigation</SheetTitle>
+        <Link href="/" className="flex items-center gap-1">
+          <Image
+            src="/images/site-logo.svg"
+            width={23}
+            height={23}
+            alt="Logo"
+          />
+          <p
+            className="h2-bold font-space-grotesk
+        text-dark-100
+        dark:text-light-900"
+          >
+            Dev <span className="text-primary-500">Flow</span>
+          </p>
+        </Link>
       </SheetContent>
     </Sheet>
   );
