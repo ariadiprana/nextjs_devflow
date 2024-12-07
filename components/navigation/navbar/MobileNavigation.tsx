b7de9f635365edabbe5a3a96df96f444ea9df9d4
ariadiprana <ari.adiprana@gmail.com>
2024-12-07 07:54:04 +0700

    MobileNavigation

--- components/navigation/navbar/MobileNavigation.tsx
+++ components/navigation/navbar/MobileNavigation.tsx
@@ -2,14 +2,17 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
+import { Button } from "@/components/ui/button";
 import {
   Sheet,
+  SheetClose,
   SheetContent,
   SheetDescription,
   SheetHeader,
   SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet";
+import ROUTES from "@/contants/routes";
 
 const MobileNavigation = () => {
   return (
@@ -43,6 +46,22 @@ const MobileNavigation = () => {
             Dev <span className="text-primary-500">Flow</span>
           </p>
         </Link>
+        <div className="no-scrollbar flex h-[calc(100vh-80px)] flex-col justify-between overflow-y-auto">
+          <SheetClose asChild>
+            <section className="flex h-full flex-col gap-6 pt-16">
+              <p>Nav Links</p>
+            </section>
+          </SheetClose>
+          <div className="flex flex-col gap-3">
+            <SheetClose asChild>
+              <Link href={ROUTES.SIGN_IN}>
+                <Button className="small-medium btn-secondary min-h-[41px] w-full rounded-lg px-4 py-3 shadow-none">
+                  <span className="primary-text-gradient">Log In</span>
+                </Button>
+              </Link>
+            </SheetClose>
+          </div>
+        </div>
       </SheetContent>
     </Sheet>
   );
